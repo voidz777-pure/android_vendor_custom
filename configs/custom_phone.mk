541715a79649e0113d17de395fa31a09e2d720cc
Steve Kondik <shade@chemlab.org>
2014-02-04 22:59:01 -0800

    custom_phone.mk: persist.sys.dun.override=0

--- configs/custom_phone.mk
+++ configs/custom_phone.mk
@@ -26,3 +26,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
 
+# Thank you, please drive thru!
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.dun.override=0
